40e012935372fe88bef354802cebe806c9e99d40
BZ2116 <116424952+BZ2116@users.noreply.github.com>
2022-12-18 10:36:57 +0800

    Add files via upload

---
+++ 练习1.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s string
+	fmt.Scanln(&s)
+	s1 := []rune(s)
+	for i := 0; i < len(s1)/2; i++ {
+		if s1[i] != s1[len(s1)-1-i] {
+			fmt.Println("false")
+			break
+		} else if i == len(s1)/2-1 {
+			switch {
+			case i%2 == 0:
+				s1 = s1[0 : len(s1)/2]
+			case i%2 == 1:
+				s1 = s1[0 : len(s1)/2+1]
+			}
+
+			s3 := string(s1)
+			fmt.Println(s3)
+		}
+
+	}
+}
